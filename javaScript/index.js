c70a1eda5f6e3cbdc269c68d4efcaabb2813c6a6
yegussss <yegus2004@gmail.com>
2023-02-14 08:58:53 -0600

    Trabajo terminado. Sólo falta twitter

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,5 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 //import { getFirestore, collection, addDoc, getDoc } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -8,40 +17,42 @@ const firebaseConfig = {
   projectId: "practica1fb",
   storageBucket: "practica1fb.appspot.com",
   messagingSenderId: "837573202124",
-  appId: "1:837573202124:web:bb9e4e0624843dc2fe7269"
+  appId: "1:837573202124:web:bb9e4e0624843dc2fe7269",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const client = document.getElementById("usuario")
+const client = document.getElementById("usuario");
 const create = document.getElementById("sign-in");
-const logged = document.getElementById("log-in")
-const nologged = document.getElementById("log-off")
+const logged = document.getElementById("log-in");
+const nologged = document.getElementById("log-off");
 const email = document.getElementById("mail");
 const pass = document.getElementById("pass");
-const googleBut = document.getElementById("googleCon")
+const googleBut = document.getElementById("googleCon");
 const providerGoogle = new GoogleAuthProvider();
+const facebookBut = document.getElementById("faceCon");
+const providerFacebook = new FacebookAuthProvider();
 
 create.addEventListener("click", function () {
-    createUserWithEmailAndPassword(auth, email.value, pass.value)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        alert("Usuario registrado con exito.")
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(error)
-        // ..
-      });
+  createUserWithEmailAndPassword(auth, email.value, pass.value)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      alert("Usuario registrado con exito.");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(error);
+      // ..
+    });
 });
 
-logged.addEventListener("click", function(){
-    signInWithEmailAndPassword(auth, email.value, pass.value)
+logged.addEventListener("click", function () {
+  signInWithEmailAndPassword(auth, email.value, pass.value)
     .then((userCredential) => {
-      // Signed in 
+      // Signed in
       const user = userCredential.user;
       alert("Usuario aunteticado.");
       // ...
@@ -51,14 +62,14 @@ logged.addEventListener("click", function(){
       const errorMessage = error.message;
       alert(error);
     });
-})
+});
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
     const uid = user.uid;
-    client.innerText = "Usuario conectado: " + user.email
+    client.innerText = "Usuario conectado: " + user.email;
     // ...
   } else {
     // User is signed out
@@ -68,35 +79,65 @@ onAuthStateChanged(auth, (user) => {
 });
 
 nologged.addEventListener("click", function () {
-  createUserWithEmailAndPassword(auth, email.value, pass.value)
-  signOut(auth).then(() => {
-    // Sign-out successful.
-    alert("La sesión ha sido cerrada")
-  }).catch((error) => {
-    // An error happened.
-  });
+  createUserWithEmailAndPassword(auth, email.value, pass.value);
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      alert("La sesión ha sido cerrada");
+    })
+    .catch((error) => {
+      // An error happened.
+    });
 });
 
-googleBut.addEventListener("click", function(){
+googleBut.addEventListener("click", function () {
   signInWithPopup(auth, providerGoogle)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-})
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+});
+
+facebookBut.addEventListener("click", function () {
+  const auth = getAuth();
+  signInWithPopup(auth, providerFacebook)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
+
+      // ...
+    });
+});
 
 /*
 // Initialize Firebase
